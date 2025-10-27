a40612037dc31bfb7b2b1341fa9916e15741a019
Suhail <suhailjho139@gmail.com>
2025-10-27 18:33:05 +0530

    twnty-third

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,14 @@ const geistSans = Geist({
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1.0,
-  userScalable: false,
-
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: "no",
+  viewportFit: "cover",
 };
+
+
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
