fe3d17b6b7115eaa4d7b1b89b32be3206750b98b
Suhail <suhailjho139@gmail.com>
2026-05-18 23:43:20 +0530

    fix caching system on projects listing page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,11 +1,14 @@
 // src/app/projects/page.js
+export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store"
+
 import Projects from "@/component/Projects"
 import { getProjects } from "@/lib/getProjects"
-const Page=async()=>{
+const Page = async () => {
   const projects = await getProjects()
-  return(
+  return (
     <div>
-      <Projects allProjects={projects}/>
+      <Projects allProjects={projects} />
     </div>
   )
 }
